1652211a37788f514da8bf33c75e9ab39ace657e
Francisco Capitan <franciscobac@hotmail.com>
2020-09-16 00:20:46 -0300

    POST records

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Game, Long> {
+
+	Record save(Record entity);
 
 }
