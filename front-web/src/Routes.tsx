bd888e370e667a251f51315bd1a6a1c091a8e961
Francisco Capitan <franciscobac@hotmail.com>
2020-09-18 03:52:39 -0300

    Adicionando a página de gráficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,17 +3,22 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Records from './pages/Records';
+import Charts from './pages/Charts';
 
 const Routes = () => (
 <BrowserRouter>
     <Header />
-    <Switch></Switch>
+    <Switch>
         <Route path="/" exact>
             <Home />
         </Route>
         <Route path="/records">
             <Records />
         </Route>
+        <Route path="/charts">
+            <Charts />
+        </Route>
+    </Switch>
 </BrowserRouter>
 );
 
